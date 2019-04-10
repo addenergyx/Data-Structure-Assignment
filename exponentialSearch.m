2973725fdf458fe8640c385fe7f96c1731568e53
David <david-adeniji@hotmail.co.uk>
2019-04-10 14:02:30 +0100

    Exponential Search partly complete
    fix dotted lines

--- exponentialSearch.m
+++ exponentialSearch.m
@@ -2,30 +2,36 @@
 
 function [numComparisons, targetIndex] = exponentialSearch(V, target)
 
-%If x is present at  
-%first location itself 
-if (V(0) == target) 
-    targetIndex = 0; 
-end
-
-numComparisons = 0;
+numComparisons = 1; % numComparisons = 0;
 arrayLength    = length(V);
 indexStart     = 1; % left
 indexEnd       = length(V); % right
+
+
+%If x is present at  
+%first location itself 
+if (V(1) == target)
+    %numComparisons = numComparisons + 1;
+    return 
+end
 
 %Find range for binary search  
 %by repeated doubling 
 i = 1;
 while(i < arrayLength && V(i) <= target)
-    %numComparisons = numComparisons + 1;
+    numComparisons = numComparisons + 1;
     i = (i * 2);
 end
 
-range = min(i,arrayLength
+[binaryNumComparisons, target] = binarySearch( V, target); % Get binary number of comparisons
+numComparisons = numComparisons + binaryNumComparisons;
 
-if (range >= 1)
-    mid = 1 + (range - 1) / 2;
-end
+
+% range = min(i,arrayLength);
+% 
+% if (range >= 1)
+%     mid = 1 + (range - 1) / 2;
+% end
     
 end
 
